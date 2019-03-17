744153ccefe435a5917a8a45ecd6ca2f992a1e00
Whitefret <nblaye@powerspace.com>
2019-03-17 11:45:04 +0100

    Add packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='welcome-bot',
+   version='1.0',
+   description='A simple bot to welcome newcomers',
+   author='Whitefret',
+   author_email='whitefret@gmail.com',
+   packages=['welcome'],
+   install_requires=[],
+)
